84803daaae8e8ea6b94cc117ccb14d69760c9b4d
Yolk Huang <yolk.tw@gmail.com>
2016-10-02 00:13:15 +0800

    Initial project

---
+++ FunctionalReactivePixels/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FunctionalReactivePixels
+//
+//  Created by Chen-An Huang on 24/09/2016.
+//  Copyright © 2016 Yolk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FunctionalReactivePixels/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FunctionalReactivePixels
+//
+//  Created by Chen-An Huang on 24/09/2016.
+//  Copyright © 2016 Yolk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
