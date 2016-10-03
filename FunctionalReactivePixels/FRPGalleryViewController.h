8336adf9fd01c2d3e3b7e0e47499d423627d88a6
Yolk Huang <yolk.tw@gmail.com>
2016-10-03 23:12:03 +0800

    Add FRPGalleryViewController class

---
+++ FunctionalReactivePixels/FRPGalleryViewController.h
@@ -0,0 +1,13 @@
+//
+//  FRPGalleryViewController.h
+//  FunctionalReactivePixels
+//
+//  Created by Yolk on 10/3/16.
+//  Copyright © 2016 Yolk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FRPGalleryViewController : UICollectionViewController
+
+@end
